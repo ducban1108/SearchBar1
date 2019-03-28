8bb38f227645da3406d7d1d10871cc85d6539fcb
Duc <levietduc1108@gmail.com>
2019-03-28 16:20:56 +0700

    Initial Commit

---
+++ SearchBar_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchBar_1
+//
+//  Created by Duc on 3/28/19.
+//  Copyright © 2019 Duc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
